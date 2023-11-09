841804c6b3392c7823981e831f46c7d7b5855080
brandon-schabel <brandonschabel1995@gmail.com>
2023-11-09 01:59:19 -0700

    add .env config key checks

--- src/config.ts
+++ src/config.ts
@@ -2,10 +2,34 @@ export const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET || '';
 
 export const isDev = Bun.env.NODE_ENV === 'development' || '';
 
+if (isDev) {
+  console.info('Running in development mode.');
+}
+
 export const sessionSecret = Bun.env.SESSION_SECRET || '';
 
 export const stripePublicKey = Bun.env.STRIPE_PUBLIC_KEY || '';
 export const stripeSecretKey = Bun.env.STRIPE_SECRET_KEY || '';
 
-export const webhookEndpoint = Bun.env.STRIPE_WEBHOOK || '';
 export const hostURL = Bun.env.HOST_URL || '';
+
+const config = {
+  endpointSecret,
+  isDev,
+  sessionSecret,
+  stripePublicKey,
+  stripeSecretKey,
+  hostURL,
+};
+
+const configKeys = Object.keys(config) as (keyof typeof config)[];
+
+if (isDev) {
+  console.info(new Date().toISOString());
+}
+
+configKeys.forEach((key) => {
+  if (!config[key]) {
+    throw new Error(`Missing required environment variable: ${key}`);
+  }
+});
