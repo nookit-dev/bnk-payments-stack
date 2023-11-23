d500df76234845459c36de545e68121ebed49406
brandon-schabel <brandonschabel1995@gmail.com>
2023-11-23 13:27:48 -0700

    update setup-fly.sh

--- setup-fly.sh
+++ setup-fly.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # Define the repository URL
-REPO_URL="https://raw.githubusercontent.com/brandon-schabel/bun-nook-kit/main/utils/setup-scripts/fly-deploy-files"
+REPO_URL="https://raw.githubusercontent.com/brandon-schabel/bun-nook-kit/main/utils/setup-scripts/payments-stack/fly-deploy-files"
 
 # Download the files
 curl -O "$REPO_URL/.dockerignore"
 curl -O "$REPO_URL/deploy-to-fly.yml"
 curl -O "$REPO_URL/Dockerfile"
 curl -O "$REPO_URL/fly.toml"
+curl -O "$REPO_URL/fly-readme.md"
 
 # Move deploy-to-fly.yml to .github/workflows in the current working directory
 mkdir -p .github/workflows
