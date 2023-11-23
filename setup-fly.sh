43c0ceb1bad8a4345f1ad11638720ed9233cfb0e
brandon-schabel <brandonschabel1995@gmail.com>
2023-11-23 14:05:40 -0700

    create fly setup script

--- setup-fly.sh
+++ setup-fly.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
+PROJECT_DIR=$(pwd)
 
-# Define the repository URL
-REPO_URL="https://raw.githubusercontent.com/brandon-schabel/bun-nook-kit/main/utils/setup-scripts/payments-stack/fly-deploy-files"
-
-# Download the files
-curl -O "$REPO_URL/.dockerignore"
-curl -O "$REPO_URL/deploy-to-fly.yml"
-curl -O "$REPO_URL/Dockerfile"
-curl -O "$REPO_URL/fly.toml"
-curl -O "$REPO_URL/fly-readme.md"
-
-# Move deploy-to-fly.yml to .github/workflows in the current working directory
-mkdir -p .github/workflows
-mv deploy-to-fly.yml .github/workflows/
-
-echo "Files have been moved successfully."
-
-echo "If you don't already have fly installed"
-echo "install with: brew install flyctl"
-echo "or with curl/linux: curl -L https://fly.io/install.sh | sh"
-echo "Now that you are setup with fly, you can delete this file."
+# run dir, get current dir and pass to setup.sh as --dir argument
+sh ./node_modules/bnkit/utils/setup-scripts/fly-deploy-files/setup.sh --dir $PROJECT_DIR
