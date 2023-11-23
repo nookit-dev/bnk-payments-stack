14ffdbe67cfd7becfc91d95b00dc4b61aac179af
brandon-schabel <brandonschabel1995@gmail.com>
2023-11-23 16:13:36 -0700

    fix cli issues

--- bnk-cli.sh
+++ bnk-cli.sh
@@ -2,4 +2,4 @@
 PROJECT_DIR=$(pwd)
 
 # run dir, get current dir and pass to setup.sh as --dir argument
-sh ./node_modules/bnkit/utils/setup-scripts/fly-deploy-files/setup.sh --dir $PROJECT_DIR
+sh ./node_modules/bnkit/scripts/cli.sh --dir $PROJECT_DIR
